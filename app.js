c18caa2917ac3b290459f7e374a9c0f76d9b48eb
Sapna <sapnakashyap2109@gmail.com>
2022-05-24 20:30:41 +0530

    4th commit

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let userInputs =["Shweta" , "Simran" , "Ravi"]
+let originalStory = `For every week the ${userInputs[0]} go  on Walk , I plant a row of sunflower seeds on ${userInputs[0]}  Pot. Before long, two friend of her ${userInputs[1]}  and ${userInputs[2]}  come her house to take ${userInputs[0]}  that  turn into a nightmare . After a while, happy times begins.Soon, golden yellow times are everywhere. Whenever someone asks how they are so happy and bloomed, 
+${userInputs[0]}  shake her head and say, A simply incident changed everything.`;
+// "For every week the Shewta go  on Walk , I plant a row of sunflower seeds on Shewta Pot. Before long, two rfriend of her Simran amnd Ravi come her house to take her that  turn into a nightmare . After a while, happy times begins.Soon, golden yellow times are everywhere. Whenever someone asks how they are so happy and bloomed, 
+//  Shetwa shake her head and say, A simply incident too many." ;
+
+
+
+console.log (originalStory);
